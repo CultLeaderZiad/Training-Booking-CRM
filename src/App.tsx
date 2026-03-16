7d5f0b5cba3e58c2f4497ceaf760dfe8f5e4735d
ziad sabry <cultleaderziad@gmail.com>
2026-03-16 21:05:19 +0200

    Enabled Web Analytics - D

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -34,6 +35,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <Analytics />
           <VoiceflowChat />
           <Routes>
             <Route path="/" element={<Index />} />
